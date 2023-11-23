cbd23063b32d3dd406b87d0c9c2770cf2aa6ea6d
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2023-11-23 15:09:36 +0530

    Delete routes directory

--- routes/temperature_Sensor.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const temperature_sensor_router=express.Router();
-temperature_sensor_router.get('/',(req,res)=>{
-    res.status(200).send("in temperature_sensor");
-
-});
-module.exports=temperature_sensor_router;
--- routes/ultrasonic_sensor.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const ultrasonic_sensor_router=express.Router();
-ultrasonic_sensor_router.get('/',(req,res)=>{
-    res.status(200).json({
-        message:"success",
-        data:"yltrasonic"
-    })
-});
-module.exports=ultrasonic_sensor_router;
